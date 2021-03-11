30793861e6eb924f6e28a496c77f287ff85e3bda
Francisco Martinez <panchetz7@gmail.com>
2021-03-11 00:36:44 -0800

    add navbar & background

---
+++ src/components/Background.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import image from './2021-goals.jpg';
+
+const Background = () => {
+  return <img className='responsive-img' src={image} alt='Image' />;
+};
+
+export default Background;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+const Navbar = () => {
+  return (
+    <div className='navbar-fixed'>
+      <div className='nav-wrapper'>
+        <h2 className='brand-logo center red lighten-3'>Get Err Done</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
